21295ca5765b6c8c3a963babd86879b2c4672584
warp-core <warp-core@users.noreply.github.com>
2022-05-25 00:27:17 +0100

    That was not fun.

--- source/Hardpoint.h
+++ source/Hardpoint.h
@@ -33,6 +33,7 @@ class Visual;
 class Hardpoint {
 public:
 	// Constructor. Hardpoints may or may not specify what weapon is in them.
+	Hardpoint(const Hardpoint &hardpoint);
 	Hardpoint(const Point &point, const Angle &baseAngle, bool isTurret,
 		bool isParallel, bool isUnder, const Outfit *outfit = nullptr,
 		bool isLocked = false, bool isDefensive = false, bool isOpportunistic = false,
@@ -112,6 +113,23 @@ public:
 	void SetIndividualAFMode(bool input);
 
 	void CycleAutoFireMode();
+	void SwapFireControlConfigs(Hardpoint &other);
+
+
+private:
+	class FireControlConfig {
+	public:
+		FireControlConfig(bool isLocked = false, bool isDefensive = false, bool isOpportunistic = false, bool isAutoFireOn = true, bool frugalAutoFire = false, bool hasIndividualAFMode = false);
+		// Copy constructor
+		FireControlConfig(FireControlConfig &input);
+
+		bool isLocked = false;
+		bool isDefensive = false;
+		bool isOpportunistic = false;
+		bool isAutoFireOn = true;
+		bool frugalAutoFire = false;
+		bool hasIndividualAFMode = false;
+	};
 
 
 private:
@@ -134,18 +152,7 @@ private:
 	bool isUnder = false;
 
 	// Indicates whether fire control attributes on this hardpoint should be editable,
-	// including by uninstallation.
-	bool isLocked = false;
-	// Indicates whether the hardpoint is set to defensive mode.
-	bool isDefensive = false;
-	// Indicates whether the hardpoint is set to opportunistic. Only applies to turrets.
-	bool isOpportunistic = false;
-	// Indicates whether the hardpoint is allowed to fire.
-	bool isAutoFireOn = true;
-	// Indicates whether the hardpoint should always be frugal, regardless of preference or personality.
-	bool frugalAutoFire = false;
-	// Indicates whether the hardpoint has its own frugality setting.
-	bool hasIndividualAFMode = false;
+	FireControlConfig fireControlConfig;
 
 	// Angle adjustment for convergence.
 	Angle angle;
